2a81290f4105a0389d635dac522c754e521940d4
a-r-t <alexthimineur@gmail.com>
2020-05-18 19:27:36 -0400

    tile updates

--- src/Tilesets/CommonTileset.java
+++ src/Tilesets/CommonTileset.java
@@ -19,6 +19,16 @@ public class CommonTileset extends Tileset {
                 return new Tile(getSubImage(0,1), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
             case 2:
                 return new Tile(getSubImage(0,2), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
+            case 3:
+                return new Tile(getSubImage(0,3), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
+            case 4:
+                return new Tile(getSubImage(0,4), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
+            case 5:
+                return new Tile(getSubImage(0,5), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
+            case 6:
+                return new Tile(getSubImage(1,0), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
+            case 7:
+                return new Tile(getSubImage(1,1), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
             default:
                 return null;
         }

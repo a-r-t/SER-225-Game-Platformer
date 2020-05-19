fcb43987fe8fbc521dc4ced2f53ee96f07ea4d5d
a-r-t <alexthimineur@gmail.com>
2020-05-18 20:32:31 -0400

    simplified tile creation

--- src/Tilesets/CommonTileset.java
+++ src/Tilesets/CommonTileset.java
@@ -12,25 +12,8 @@ public class CommonTileset extends Tileset {
 
     @Override
     public Tile createTile(int tileNumber, int xIndex, int yIndex) {
-        switch (tileNumber) {
-            case 0:
-                return new Tile(getSubImage(0,0), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
-            case 1:
-                return new Tile(getSubImage(0,1), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
-            case 2:
-                return new Tile(getSubImage(0,2), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
-            case 3:
-                return new Tile(getSubImage(0,3), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
-            case 4:
-                return new Tile(getSubImage(0,4), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
-            case 5:
-                return new Tile(getSubImage(0,5), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
-            case 6:
-                return new Tile(getSubImage(1,0), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
-            case 7:
-                return new Tile(getSubImage(1,1), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
-            default:
-                return null;
-        }
+        int tileRowIndex = tileNumber / this.columnLength;
+        int tileColumnIndex = tileNumber % this.rowLength;
+        return new Tile(getSubImage(tileRowIndex, tileColumnIndex), xIndex * spriteWidth * scale, yIndex * spriteHeight * scale);
     }
 }

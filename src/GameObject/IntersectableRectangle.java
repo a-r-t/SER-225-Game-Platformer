6585953ebba010389487b84082fbecd24a228add
a-r-t <alexthimineur@gmail.com>
2020-08-23 16:54:38 -0400

    finished commenting up the GameObject package

--- src/GameObject/IntersectableRectangle.java
+++ src/GameObject/IntersectableRectangle.java
@@ -1,5 +1,7 @@
 package GameObject;
 
+// This interface allows for specifying a rectangle that can be used in intersection logic by the Rectangle class
+// it really only exists so an AnimatedSprite can be checked for intersection directly against by a Sprite or Rectangle (and all other combinations of that)
 public interface IntersectableRectangle {
     Rectangle getIntersectRectangle();
 }

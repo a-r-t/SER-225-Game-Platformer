e7f9ad32cb175ba3bb649fdabe5b71129c863a8d
KyleChutjian <kyle.chutjian@gmail.com>
2020-09-20 22:52:06 -0400

    Deleted test comment

--- src/Level/Map.java
+++ src/Level/Map.java
@@ -351,8 +351,6 @@ public abstract class Map {
 			player.handleCollisionX(-3);
 		}
 
-		// Can only see in Original (test)
-
 		if (player.getCalibratedXLocation() > xMidPoint && camera.getEndBoundX() < endBoundX) {
 			float xMidPointDifference = xMidPoint - player.getCalibratedXLocation();
 			camera.moveX(-xMidPointDifference);

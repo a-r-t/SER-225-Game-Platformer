c5537fa28226d222a811552d7ae7243dba9bc47d
a-r-t <alexthimineur@gmail.com>
2022-08-10 23:41:52 -0400

    update name of method

--- src/GameObject/AnimatedSprite.java
+++ src/GameObject/AnimatedSprite.java
@@ -124,7 +124,7 @@ public class AnimatedSprite implements IntersectableRectangle {
 	public String getCurrentAnimationName() { return this.currentAnimationName; }
 	public int getCurrentFrameIndex() { return this.currentFrameIndex; }
 
-	public void setCurrentAnimation(String animationName) {
+	public void setCurrentAnimationName(String animationName) {
 		this.currentAnimationName = animationName;
 	}
 
